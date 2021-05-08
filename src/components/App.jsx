8e680b4de071bdff2dc19335917badc4049586c2
Nurullo <khuwaidullaev@gmail.com>
2021-05-08 18:04:30 +0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function Entry(emojiEl) {
+  return (
+    <Card
+      key={emojiEl.id}
+      name={emojiEl.name}
+      emoji={emojiEl.emoji}
+      description={emojiEl.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(Entry)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🖖",
+    name: "Vulcan Salute",
+    meaning:
+      "A raised hand, with the fingers separated between the ring finger and the middle finger.",
+  },
+  {
+    id: 2,
+    emoji: "🙌",
+    name: "Raising Hands",
+    meaning:
+      "Two hands raised in the air, celebrating success or another joyous event.",
+  },
+  {
+    id: 3,
+    emoji: "🤙",
+    name: "Call Me Hand",
+    meaning:
+      "A hand with thumb and little (pinky) finger extended, making a traditional phone-like shape. May be used in conjunction with a face to suggest “call me” (please).",
+  },
+];
+
+export default emojipedia;
